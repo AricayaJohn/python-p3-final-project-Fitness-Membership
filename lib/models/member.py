62681e7ce3bdbf06b02fbd397d1490646201b350
John <jaricaya@gmail.com>
2024-10-31 00:38:32 +0800

    added class_id to be foreign key

--- lib/models/member.py
+++ lib/models/member.py
@@ -4,13 +4,14 @@ class Member:
 
     all = []
 
-    def __init__(self, name, id=None):
+    def __init__(self, name, id=None, class_id):
         self.name = name
         self.id = id
+        self.class_id  = class_id
         Member.all.append(self)
 
     def __repr__(self):
-        return f"Member name={self.name} id={self.id}"
+        return f"Member name={self.name} id={self.id} class_id = {self.class_id}"
 
     @property
     def name(self):
@@ -35,3 +36,9 @@ class Member:
             self._id = new_id
         else:
             raise TypeError("Member id must be an integer")
+
+    @property
+    def class_id(self):
+        return self._class_id
+
+    
