1f375ac6943299d83a38663a0cab80a9ef574dae
John <jaricaya@gmail.com>
2024-11-01 06:48:26 +0800

    commenting seed syntax to test save class method

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 # lib/debug.py
-from seed import seed_database
+
+from models.__init__ import CONN, CURSOR
+from models.member import Member
+from models.workout_class import Workout
+# from seed import seed_database
 
 import ipdb
 
-seed_database()
+# seed_database()
 
 ipdb.set_trace()
--- lib/seed.py
+++ lib/seed.py
@@ -4,19 +4,19 @@ from models.__init__ import CONN, CURSOR
 from models.member import Member
 from models.workout_class import Workout
 
-def seed_database():
-    Member.drop_table()
-    Workout.drop_table()
-    Workout.create_table()
-    Member.create_table()
+# def seed_database():
+#     Member.drop_table()
+#     Workout.drop_table()
+#     Workout.create_table()
+#     Member.create_table()
 
-    #create starter data
-    #member starter data
-    m1 = Member.create("John", 1)
-    m2 = Member.create("Peter", 2)
+#     #create starter data
+#     #member starter data
+#     m1 = Member.create("John", 1)
+#     m2 = Member.create("Peter", 2)
 
-    #class program starter data 
-    class_name = Workout.create("HIIT boxing", 1, 1)
+#     #class program starter data 
+#     class_name = Workout.create("HIIT boxing", 1, 1)
 
 
-seed_database()
+# seed_database()
