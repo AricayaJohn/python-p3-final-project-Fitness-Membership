7fe612c4aeed3bbcca3dbdd0c0588942217b686b
John <jaricaya@gmail.com>
2024-11-07 13:46:55 +0800

    tested seed file

--- lib/debug.py
+++ lib/debug.py
@@ -4,7 +4,7 @@
 from models.__init__ import CONN, CURSOR
 from models.member import Member
 from models.workout_class import Workout
-# from seed import seed_database
+from seed import seed_database
 
 import ipdb
 
